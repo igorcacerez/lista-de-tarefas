a3e8400915b54dc9a46ea33191534e13eb5e3e49
Igor Cacerez <igor.cacerez@gmail.com>
2024-04-28 02:39:26 -0300

    Instalacao thema Soft UI

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './theme/css/nucleo-icons.css';
+import './theme/css/soft-ui-dashboard.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
